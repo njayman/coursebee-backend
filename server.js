e4208848dbd4427b181e0a5a89b1809acef5d26f
njayman <najishm828282@gmail.com>
2020-05-01 19:44:31 +0600

    trash removal

--- server.js
+++ server.js
@@ -4,20 +4,11 @@ const bodyParser = require('body-parser');
 var dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-
-
-//for socket
-//used for realtime video upload
-const http = require('http');
-const socketio = require('socket.io');
-const server = http.createServer(app)
-const io = socketio(server);
-const PORT = process.env.PORT || 3001
-//
+const cors = require('cors')
 
 //parsers
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 
 //mongodb connection
@@ -36,6 +27,6 @@ app.use('/admin', AdminRoute, (req, res) => {
 
 
 //listening
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
 })
